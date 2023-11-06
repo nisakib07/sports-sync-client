1ae0880975c886c28cabaf3b8be45ef33f77e932
nisakib07 <websakib07@gmail.com>
2023-11-06 20:07:17 +0600

    Fixed all services

--- src/Pages/AllServices/AllServices.jsx
+++ src/Pages/AllServices/AllServices.jsx
@@ -1,21 +1,43 @@
-import ServiceCard from "../../components/ServiceCard/ServiceCard";
+import AllServicesCard from "../../components/AllServicesCard/AllServicesCard";
+
 import useServices from "../../hooks/useServices";
 
 const AllServices = () => {
   const { data, isLoading } = useServices();
+  //   const [searchedData, setSearchedData] = useState(data);
 
   if (isLoading) return <p>Loading</p>;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
       <h2 className="text-center text-2xl font-bold my-6">
         All of our Services
       </h2>
+      <div className="flex justify-center mb-6">
+        <form onSubmit={handleSearch}>
+          <div className="flex">
+            <input
+              type="text"
+              name="searchedService"
+              placeholder="Type here"
+              className="input input-bordered rounded-none
+             rounded-l-lg input-success w-full max-w-xs"
+            />
+            <button type="submit" className="bg-cyan-400 p-2 rounded-r-lg">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {data.map((singleData) => (
-          <ServiceCard
+          <AllServicesCard
             key={singleData._id}
-            singleData={singleData}></ServiceCard>
+            singleData={singleData}></AllServicesCard>
         ))}
       </div>
     </div>
---
+++ src/components/AllServicesCard/AllServicesCard.jsx
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const AllServicesCard = ({ singleData }) => {
+  const {
+    _id,
+    serviceImage,
+    serviceName,
+    serviceDescription,
+    serviceProviderImage,
+    serviceProviderName,
+    servicePrice,
+    serviceArea,
+  } = singleData;
+  return (
+    <div>
+      <div className="card bg-cyan-600 shadow-xl">
+        <figure>
+          <img className="w-full h-[450px]" src={serviceImage} />
+        </figure>
+        <div className="p-5">
+          <h2 className="text-2xl">{serviceName}</h2>
+          <p className="min-h-[100px] ">{serviceDescription}</p>
+        </div>
+        <div className="flex justify-between items-center p-5">
+          <div className="flex items-center gap-5">
+            <img
+              className="rounded-full w-[45px] h-[45px]"
+              src={serviceProviderImage}
+              alt=""
+            />
+            <p className="text-lg">{serviceProviderName}</p>
+          </div>
+          <p>
+            <span className="text-lg font-semibold">Price :</span> $
+            {servicePrice}
+          </p>
+        </div>
+        <div className="px-5">
+          <p>
+            <span className="text-lg font-semibold">Service Area : </span>
+            {serviceArea}
+          </p>
+        </div>
+
+        <div className="flex p-5 justify-end">
+          <Link to={`/serviceDetails/${_id}`}>
+            <button className="flex items-center gap-2 bg-cyan-400 p-2 rounded-lg">
+              View Detail{" "}
+              <span>
+                <BsArrowRight></BsArrowRight>
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+AllServicesCard.propTypes = {
+  singleData: PropTypes.object,
+};
+
+export default AllServicesCard;
